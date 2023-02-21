5a1e8055d9513c03b999450f9ecdde3743c55767
OSHIN <izkp44@gmail.com>
2023-02-21 23:47:59 +0700

    #10 - useContext x useState

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './App.css'
 import LoginArea from './LoginArea'
-
-const fakeUser = { username: 'nice789', fullname: 'Kitty' }
 
 const AuthContext = React.createContext()
 
 function App() {
+  const [auth, setAuth] = useState(null)
+
   return (
-    <AuthContext.Provider value={fakeUser}>
+    <AuthContext.Provider value={{ auth, setAuth }}>
       <section className="app-section">
         <div className="app-container">
           <LoginArea />
